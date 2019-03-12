40bb38a257e8cd7de2d46f274ae1a3647da38ce4
Remi Delgatte <remi.delgatte@decathlon.com>
2019-03-12 14:03:12 +0100

    step-0: Start application

--- README.md
+++ README.md
@@ -12,3 +12,14 @@ THE RULES FOR PLAYING "MEMORY"
 - Remember what was on each card and where it was.
 - The game is over when all the cards have been matched.
 
+## Step-by-step build
+
+### Step-0: Start the application
+
+Checkout branch `step-0` and run `elm-live src/Main.elm --port=1234 --open  -- --debug`
+
+In this branch, you will find a single button and a value which is randomly generated (from 1 to 100).
+
+![step-0](doc/step-0.png)
+
+Once it is done, you can go to the next step: `git checkout -f step-1`
--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,11 @@
-module Main exposing (Model, Msg(..), init, initialModel, main, subscriptions, update, view)
+module Main exposing (Model, Msg(..), initialModel, main, update, view)
 
 import Browser
-import Configuration exposing (Configuration, cardsWidth, expectedCards, levelOptions)
-import Element exposing (Element, centerX, column, layout, padding, px, row, spaceEvenly, spacing, text, width, wrappedRow)
-import Element.Border as Border
-import Element.Input as Input
-import Html exposing (Html)
-import Image exposing (Image, Status(..), buildImages, distinct, duplicateCodes, progressRendering, randomGenerator, render)
+import Html as Input exposing (Attribute, Html, div, text)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
 import Random
-import Random.List exposing (shuffle)
+import String exposing (fromInt)
 
 
 
@@ -16,25 +13,12 @@ import Random.List exposing (shuffle)
 
 
 type alias Model =
-    { configuration : Configuration
-    , images : List Image
-    }
+    { value : Int }
 
 
-initialModel : Model
-initialModel =
-    { configuration =
-        { level = 1
-        , cardsByLevel = 6
-        , codes = []
-        }
-    , images = []
-    }
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( initialModel, generateCodes )
+initialModel : () -> ( Model, Cmd Msg )
+initialModel _ =
+    ( { value = 0 }, generateValue )
 
 
 
@@ -42,163 +26,30 @@ init _ =
 
 
 type Msg
-    = Init
-    | SelectLevel Int
-    | ShuffleCodes (List Int)
-    | LoadedRandomId (List Int)
-    | Click Image
-
-
-
--- UPDATE
+    = Roll
+    | Rolled Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update message model =
-    case message of
-        Init ->
-            ( model, generateCodes )
+update msg model =
+    case msg of
+        Roll ->
+            ( model, generateValue )
 
-        -- Selecting a level should update configuration and generate new images
-        SelectLevel selectedLevel ->
-            let
-                configuration : Configuration
-                configuration =
-                    model.configuration
-            in
-            ( { model | configuration = { configuration | level = selectedLevel } }
-            , generateCodes
-            )
-
-        -- From the generated codes, aggregate to build the images codes
-        ShuffleCodes randomCodes ->
-            let
-                configuration : Configuration
-                configuration =
-                    model.configuration
-
-                expectedNumberOfCards : Int
-                expectedNumberOfCards =
-                    expectedCards configuration
-
-                distinctCodes : List Int
-                distinctCodes =
-                    configuration.codes
-                        |> List.append randomCodes
-                        |> distinct
-                        |> List.take expectedNumberOfCards
-            in
-            ( { model | configuration = { configuration | codes = distinctCodes } }
-            , case (distinctCodes |> List.length) < expectedNumberOfCards of
-                True ->
-                    generateCodes
-
-                False ->
-                    distinctCodes
-                        |> duplicateCodes
-                        |> shuffle
-                        |> Random.generate LoadedRandomId
-            )
-
-        -- From the random generated codes, we build a list of images
-        LoadedRandomId codes ->
-            ( { model | images = codes |> buildImages }, Cmd.none )
-
-        Click clickedImage ->
-            let
-                selectedImages : List Image
-                selectedImages =
-                    model.images
-                        |> List.filter (\img -> img.status == Visible)
-
-                updatedImages : List Image
-                updatedImages =
-                    case selectedImages |> List.length of
-                        0 ->
-                            model.images
-                                |> List.map
-                                    (\img ->
-                                        case img.index == clickedImage.index of
-                                            True ->
-                                                { img | status = Visible }
-
-                                            False ->
-                                                img
-                                    )
-
-                        1 ->
-                            case
-                                selectedImages
-                                    |> List.filter (\img -> img.id == clickedImage.id && img.index /= clickedImage.index)
-                                    |> List.isEmpty
-                            of
-                                True ->
-                                    model.images
-                                        |> List.map
-                                            (\img ->
-                                                case img.index == clickedImage.index of
-                                                    True ->
-                                                        { img | status = Visible }
-
-                                                    False ->
-                                                        img
-                                            )
-
-                                False ->
-                                    model.images
-                                        |> List.map
-                                            (\img ->
-                                                case img.id == clickedImage.id of
-                                                    True ->
-                                                        { img | status = Found }
-
-                                                    False ->
-                                                        img
-                                            )
-
-                        2 ->
-                            model.images
-                                |> List.map
-                                    (\img ->
-                                        case img.status == Visible of
-                                            True ->
-                                                { img | status = Hidden }
-
-                                            False ->
-                                                img
-                                    )
-                                |> List.map
-                                    (\img ->
-                                        case img.index == clickedImage.index of
-                                            True ->
-                                                { img | status = Visible }
-
-                                            False ->
-                                                img
-                                    )
-
-                        _ ->
-                            model.images
-            in
-            ( { model | images = updatedImages }
-            , Cmd.none
-            )
+        Rolled randomValue ->
+            ( { value = randomValue }, Cmd.none )
 
 
-generateCodes : Cmd Msg
-generateCodes =
-    10
-        |> randomGenerator 1 100
-        |> Random.generate ShuffleCodes
+generateValue : Cmd Msg
+generateValue =
+    generate 1 100
 
 
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
+generate : Int -> Int -> Cmd Msg
+generate min max =
+    max
+        |> Random.int min
+        |> Random.generate Rolled
 
 
 
@@ -208,91 +59,34 @@ subscriptions _ =
 view : Model -> Html Msg
 view model =
     let
-        images : Element Msg
-        images =
-            model.images
-                |> List.map
-                    (\image ->
-                        let
-                            action : Maybe Msg
-                            action =
-                                case image.status of
-                                    Hidden ->
-                                        Just (Click image)
-
-                                    _ ->
-                                        Nothing
-                        in
-                        { onPress = action
-                        , label = render image
-                        }
-                            |> Input.button []
-                    )
-                |> wrappedRow
-                    [ spacing 5
-                    , spaceEvenly
-                    , model.configuration |> cardsWidth |> px |> width
-                    ]
-
-        screen : Element Msg
-        screen =
-            [ progressRendering model.images
-            , images
-            ]
-                |> column
-                    [ padding 10
-                    , spacing 10
-                    ]
-
-        initButton =
-            { onPress = Just Init
-            , label = text "Restart"
-            }
-                |> Input.button
-                    [ padding 10
-                    , spacing 20
-                    , Border.width 1
-                    ]
-
-        levelSelector : Element Msg
-        levelSelector =
-            { onChange = SelectLevel
-            , selected = Just model.configuration.level
-            , label =
-                "Level"
-                    |> text
-                    |> Input.labelAbove []
-            , options = levelOptions
-            }
-                |> Input.radio
-                    [ padding 10
-                    , spacing 10
-                    ]
-
-        configurationSideBar : Element Msg
-        configurationSideBar =
-            [ initButton ]
-                |> List.append [ levelSelector ]
-                |> column
-                    [ centerX
-                    , Element.alignTop
-                    , padding 20
-                    ]
+        diceValue : Html Msg
+        diceValue =
+            (model.value
+                |> fromInt
+            )
+                |> String.append "Value = "
+                |> text
     in
-    [ configurationSideBar, screen ]
-        |> row [ centerX ]
-        |> layout []
+    [ diceValue
+    , Input.button [ onClick Roll, style "margin" "5px" ] [ text "Generate" ]
+    ]
+        |> div []
 
 
 
--- MAIN
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = init
-        , update = update
+        { init = initialModel
         , view = view
+        , update = update
         , subscriptions = subscriptions
         }
